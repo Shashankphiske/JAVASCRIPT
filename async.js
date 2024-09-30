b070e5810154c2b66648df2983ad2f5577f2f080
ShashankPhiske <shashankphiske@gmail.com>
2024-09-30 22:59:09 +0530

    30092024

---
+++ async.js
@@ -0,0 +1,3 @@
+// the default js is synchronous and single threaded therefore it waits for the previous line of the code to process
+// and then shifts to another new line of code (each new operation waits for the last on to complete)
+
